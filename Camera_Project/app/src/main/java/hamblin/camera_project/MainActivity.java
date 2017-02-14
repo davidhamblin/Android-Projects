008d6561d2ba7a92e394ea815aea91be6485c73d
David Hamblin <david.hamblin.12@cnu.edu>
2017-02-14 11:22:46 -0500

    Added project 2 shell code

---
+++ Camera_Project/app/src/main/java/hamblin/camera_project/MainActivity.java
@@ -0,0 +1,39 @@
+package hamblin.camera_project;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent myIntent = new Intent(this, SettingsActivity.class);
+                startActivity(myIntent);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+}
+
