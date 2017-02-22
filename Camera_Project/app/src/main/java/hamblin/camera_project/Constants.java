8c87cfd3340e6df453a54dd6857a4888674aab5a
David Hamblin <david.hamblin.12@cnu.edu>
2017-02-22 11:06:43 -0500

    Added Constants file, started Graduate project

---
+++ Camera_Project/app/src/main/java/hamblin/camera_project/Constants.java
@@ -0,0 +1,14 @@
+package hamblin.camera_project;
+
+/**
+ * Created by david on 2/16/2017.
+ */
+
+public class Constants {
+    protected static final int TAKE_PICTURE = 12;
+    protected static final int CAMERA_REQUEST = 50;
+    protected static final String IO_TAG = "IO";
+    protected static final String filename = "tempPic";
+    protected static final String extension = ".bmp";
+    protected static final String full_file = "/" + filename + extension;
+}
