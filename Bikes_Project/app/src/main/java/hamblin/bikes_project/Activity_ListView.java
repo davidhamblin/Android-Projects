5e0ba4b6301858da6c51f2de45e46d7df3a8807e
David Hamblin <david.hamblin.12@cnu.edu>
2017-03-30 18:19:26 -0400

    Added toolbar and action buttons to project 4

--- Bikes_Project/app/src/main/java/hamblin/bikes_project/Activity_ListView.java
+++ Bikes_Project/app/src/main/java/hamblin/bikes_project/Activity_ListView.java
@@ -1,12 +1,25 @@
 package hamblin.bikes_project;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_ListView extends AppCompatActivity {
 	ListView my_listview;
@@ -25,7 +38,6 @@ public class Activity_ListView extends AppCompatActivity {
 		//toolbar
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		android.support.v7.app.ActionBar actionBar = getSupportActionBar();
 
 		setupSimpleSpinner();
 
@@ -60,8 +72,29 @@ public class Activity_ListView extends AppCompatActivity {
 	 * dontforget to bind the listener to the spinner with setOnItemSelectedListener!
 	 */
 	private void setupSimpleSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        List<String> bikeNames = new ArrayList<>();
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.sortable_fields, R.layout.spinner_item);
+        spinner.setVisibility(View.VISIBLE);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            static final int SELECTED_ITEM = 0;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getChildAt(SELECTED_ITEM) != null) {
+                    // Sort according to selection. Use comparators.
+//                    ((TextView) parent.getChildAt(SELECTED_ITEM)).setTextColor(Color.WHITE);
+                    Toast.makeText(Activity_ListView.this, (String) parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -72,11 +105,42 @@ public class Activity_ListView extends AppCompatActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case 0:
-				break;
+			case R.id.action_settings:
+                Intent myIntent = new Intent(this, activityPreference.class);
+                startActivity(myIntent);
+                break;
+            case R.id.action_about:
+                showAbout();
+                break;
+            case R.id.action_refresh:
+                break;
 			default:
 				break;
 		}
 		return true;
 	}
+
+    /**
+     * Shows personal/project information in an About dialog opened from the overflow menu
+     */
+    private void showAbout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("David Hamblin")
+                .setMessage("Project 4, Bikes Project\nGraduate student in CPSC 575\nRad dude overall");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Snackbar.make(findViewById(R.id.activity_main), "Thanks for reading my About!", Snackbar.LENGTH_LONG)
+                        .setAction("CLOSE", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(Activity_ListView.this, "A bit hasty aren't we?", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+//        builder.setIcon(R.drawable.david);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
